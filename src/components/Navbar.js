c3fc45cc908e9f21a4be7b87b987101a46792b6b
Ritvik Dayal <ritvikr1605@gmail.com>
2020-12-06 14:10:43 +0530

    updated navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -29,8 +29,8 @@ function Navbar() {
       <nav className='navbar'>
         <div className='navbar-container'>
           <Link to='/' className='navbar-logo' onClick={closeMobileMenu}>
-            TRVL
-            <i class='fab fa-typo3' />
+            RCreations &nbsp;
+            <i class='fa fa-fighter-jet' />
           </Link>
           <div className='menu-icon' onClick={handleClick}>
             <i className={click ? 'fas fa-times' : 'fas fa-bars'} />
