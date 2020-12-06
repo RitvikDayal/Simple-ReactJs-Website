3163bd4a9cade8067de54ff73995b515ba627970
Ritvik Dayal <ritvikr1605@gmail.com>
2020-12-06 15:13:09 +0530

    added cards

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -8,8 +8,39 @@ function Cards() {
             <h1>Check out these travel pictures!</h1>
             <div className='cards__container'>
                 <div className='cards__wrapper'>
+                <ul className='cards__items'>
+                        <CardItem 
+                        src="images/img-1.jpg"text="Explore the hidden Waterfall deep inside the Amazon jungle"
+                        label="Adventure"
+                        path="/services"
+                        />
+                        <CardItem 
+                        src="images/img-2.jpg"text="Explore the hidden Waterfall deep inside the Amazon jungle"
+                        label="Luxury"
+                        path="/products"
+                        />
+                    </ul>
                     <ul className='cards__items'>
-                        <CardItem />
+                        <CardItem 
+                        src="images/img-3.jpg"text="Explore the hidden Waterfall deep inside the Amazon jungle"
+                        label="Adventure"
+                        path="/services"
+                        />
+                        <CardItem 
+                        src="images/img-4.jpg"text="Explore the hidden Waterfall deep inside the Amazon jungle"
+                        label="Luxury"
+                        path="/products"
+                        />
+                        <CardItem 
+                        src="images/img-5.jpg"text="Explore the hidden Waterfall deep inside the Amazon jungle"
+                        label="Luxury"
+                        path="/products"
+                        />
+                        <CardItem 
+                        src="images/img-6.jpg"text="Explore the hidden Waterfall deep inside the Amazon jungle"
+                        label="Luxury"
+                        path="/products"
+                        />
                     </ul>
                 </div>
             </div>
