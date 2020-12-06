5de386c92e717ab62892c4dc87e6becb2fb1b21b
Ritvik Dayal <ritvikr1605@gmail.com>
2020-12-06 14:11:44 +0530

    added Hero Section

---
+++ src/components/HeroSection.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import '../App.css';
+import { Button } from './Button';
+import './HeroSection.css';
+
+function HeroSection() {
+  return (
+    <div className='hero-container'>
+      <video src='/videos/video-1.mp4' autoPlay loop muted />
+      <h1>One Page Wonder</h1>
+      <p>Will Rock Your Socks Off</p>
+      <div className='hero-btns'>
+        <Button
+          className='btns'
+          buttonStyle='btn--outline'
+          buttonSize='btn--large'
+        >
+          LEARN MORE
+        </Button>
+      </div>
+    </div>
+  );
+}
+
+export default HeroSection;
